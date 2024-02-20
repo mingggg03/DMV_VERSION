f9d57dd532766625594eb62f423aabb91527c336
Legion <minhnhph22720@fpt.edu.vn>
2024-02-20 20:58:34 +0700

    add cuoi

---
+++ src/main/java/fpoly/duantotnghiep/shoppingweb/controller/user/CheckOutController.java
@@ -0,0 +1,22 @@
+package fpoly.duantotnghiep.shoppingweb.controller.user;
+
+import fpoly.duantotnghiep.shoppingweb.service.impl.GioHangServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CheckOutController {
+    @Autowired
+    GioHangServiceImpl gioHangService;
+
+
+    @GetMapping("/check-out")
+    public String checkOut() {
+        if (gioHangService.laySpTrongGio() == null) {
+            return "redirect:/cart";
+
+        }
+        return "/user/Checkout";
+    }
+}
